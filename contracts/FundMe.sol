40f6805bf4e4e0b1b83f06dcd99d6b0d7ce78da2
tdhluminous <toddh@luminousdap.com>
2023-12-13 15:42:02 -0600

    fundme

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -10,11 +10,12 @@ pragma solidity ^0.8.18;
 contract FundMe {
 
     uint256 public myValue = 1;
+    uint256 public minimumUsd = 5;
 
     function fund() public payable  {
 
         myValue = myValue + 2;
-        require(msg.value >= 1e18, "must send at least 1 ETH"); // 1e18 = 1 eth also = 1000000000000000000 gwei
+        require(msg.value >= minimumUsd, "must send at least 5 dollars"); // 1e18 = 1 eth also = 1000000000000000000 gwei
 
 
     }
