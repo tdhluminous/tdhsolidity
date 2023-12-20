35e439919028977473cdc152fbef9e31f3dc8f1a
tdhluminous <toddh@luminousdap.com>
2023-12-20 12:49:35 -0600

    fundme update

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -8,45 +8,45 @@
 
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
 
-    
+    using PriceConverter for uint256;
+
     uint256 public minimumUsd = 5e18;
 
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
     function fund() public payable  {
+        msg.value.getConversionRate();
 
        
-        require(getConversionRate(msg.value) >= minimumUsd, "did not send enough ETH"); 
+        require(msg.value.getConversionRate() >= minimumUsd, "did not send enough ETH"); 
 //      require(msg.value >= 1e18);   1e18 = 1 eth also = 1000000000000000000 gwei
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        
 
 
     }
 
-//    function withdraw() public {}
+    function withdraw() public {
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex = funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
 
-  function getPrice() public view returns(uint256){
-   AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-   (uint80 roundId, int256 price, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) = priceFeed.latestRoundData();
- 
-   return uint256(price * 1e10);
-}
-
-
-    function getConversionRate(uint256 ethAmount) public view returns(uint256){
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;  
-        return ethAmountInUsd;
-
+// reset the funders array
+        funders = new address[] (0);
 
     }
 
-    function getVersion() public view returns (uint256){
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
-    }
+// withdraw the funds
 
-
+ // you must wrap msg.sender address in payable typecast or it cannot receive funds
+    payable(msg.sender).transfer(address(this).balance);
 
 
 
