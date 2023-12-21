c06ba0ab865af2d7f14169d141292150cc8c01d2
tdhluminous <toddh@luminousdap.com>
2023-12-21 15:22:25 -0600

    fundme update

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -18,6 +18,11 @@ contract FundMe {
 
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function fund() public payable  {
         msg.value.getConversionRate();
@@ -32,7 +37,8 @@ contract FundMe {
 
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner {
+        
         for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex = funderIndex++){
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
@@ -41,13 +47,26 @@ contract FundMe {
 // reset the funders array
         funders = new address[] (0);
 
-    }
-
-// withdraw the funds
+        // withdraw the funds
 
  // you must wrap msg.sender address in payable typecast or it cannot receive funds
     payable(msg.sender).transfer(address(this).balance);
+    // send
+    bool sendSuccess = payable(msg.sender).send(address(this).balance);
+    require(sendSuccess, "Send Failed");
+    // call
+    (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+    require(callSuccess, "Call failed");
 
+    }
+
+
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Sender is not owner!");
+        _;
+
+    }
 
 
 }
